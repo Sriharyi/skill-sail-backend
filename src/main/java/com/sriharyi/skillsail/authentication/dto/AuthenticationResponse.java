fe3b98ac475922f79a65d3f1689b679b42e8016d
Sriharyi <sriharyi.c@gmail.com>
2024-02-27 18:36:59 +0530

    Refractor in authication response and RegisterRequest

--- src/main/java/com/sriharyi/skillsail/authentication/dto/AuthenticationResponse.java
+++ src/main/java/com/sriharyi/skillsail/authentication/dto/AuthenticationResponse.java
@@ -12,9 +12,9 @@ import lombok.NoArgsConstructor;
 @Builder
 public class AuthenticationResponse {
 
-    @JsonProperty("access_token")
+//    @JsonProperty("access_token")
     private String accessToken;
 
-    @JsonProperty("refresh_token")
+//    @JsonProperty("refresh_token")
     private String refreshToken;
 }
--- src/main/java/com/sriharyi/skillsail/authentication/dto/RegisterRequest.java
+++ src/main/java/com/sriharyi/skillsail/authentication/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.sriharyi.skillsail.authentication.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RegisterRequest {
     private String email;
 
